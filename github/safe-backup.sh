35d18d1083794b99987a434c1f54d37d0d624f75
Matyas Selmeci <matyas@cs.wisc.edu>
2017-08-01 13:05:40 -0500

    Fix fetch config for wip/* and make it unconditional

--- github/safe-backup.sh
+++ github/safe-backup.sh
@@ -50,10 +50,7 @@ initrepo () {
 
     # allow non-ff pull request updates, and ff-only updates for all other refs
     git config remote.origin.fetch '+refs/pull/*:refs/pull/*'
-    # matyas reserves the right to force-push to wip/* branches
-    if [[ $remote =~ matyasselmeci ]]; then
-        git config --add remote.origin.fetch '+refs/wip/*:refs/wip/*'
-    fi
+    git config --add remote.origin.fetch '+refs/heads/wip/*:refs/heads/wip/*'
     git config --add remote.origin.fetch 'refs/*:refs/*'
   )
 }
